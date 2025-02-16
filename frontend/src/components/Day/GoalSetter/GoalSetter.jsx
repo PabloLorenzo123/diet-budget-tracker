8acd541be96618c25281ac76e14bc0bf016ce89a
Pablo Lorenzo <pabloernesto802@gmail.com>
2025-02-16 15:27:43 -0400

    Fixed an import statement in the frontend.

--- frontend/src/components/Day/GoalSetter/GoalSetter.jsx
+++ frontend/src/components/Day/GoalSetter/GoalSetter.jsx
@@ -6,7 +6,7 @@ import { validateDecimalNumberInput, roundTo } from "../../../lib/functions";
 import { toast } from "react-toastify";
 import api from "../../../api";
 
-import '../../../styles/diary/GoalSetter.css';
+import '../../../styles/diary/goalSetter.css';
 import '../../../styles/nutrientTableForm.css';
 
 const GoalSetter = ({dailyTargets, setDailyTargets}) => {
